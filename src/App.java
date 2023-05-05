8c522ee250f185090f87ba325a87ce2e5646b6dc
eliyya <tecno_eli@hotmail.com>
2023-05-05 00:55:03 -0600

    remove comment

--- src/App.java
+++ src/App.java
@@ -14,7 +14,6 @@ import java.util.List;
 import java.util.Scanner;
 
 public class App {
-    // lista de alumnos
     public Scanner scanner = new Scanner(System.in);
     public List<Carrera> carreras = new ArrayList<>();
 
